6623ab0720a52b09fbadbde92a9dc6d6aea879b0
David González Vaquero <dgonza46@gmail.com>
2014-06-17 16:20:03 +0200

    ATTITUDE_3

--- js/ATTITUDE_3.js
+++ js/ATTITUDE_3.js
@@ -1,7 +1,8 @@
 var animation = (function () {
 	var anim = {};
 
-	var grupoPiramide = null, bloqueOcultaPiramide = null, bordeOjo = null, rellonoOjo = null, grupoOjoInt = null, grupoPestanas = null, grupoMano = null, intervalPrimeraParte = null, intervalSegundaParte = null, intervalTerceraParte = null,
+	var grupoPiramide = null, bloqueOcultaPiramide = null, bordeOjo = null, rellonoOjo = null, grupoOjoInt = null, grupoPestanas = null, grupoMano = null, 
+        intervalPrimeraParte = null, intervalSegundaParte = null, intervalTerceraParte = null, intervalCierreCuartaParte = null, intervalCierreTerceraParte = null, intervalCierreSegundaParte = null, intervalCierreOjo = null, intervalCierreMano = null, 
 		loaded = false;
 
 	// Animation Login
@@ -52,6 +53,17 @@ var animation = (function () {
         anim.pause.grupoOjoInt = grupoOjoInt.stop();
         anim.pause.grupoPestanas = grupoPestanas.stop();
         anim.pause.grupoMano = grupoMano.stop();
+
+        clearInterval(intervalPrimeraParte);
+        clearInterval(intervalSegundaParte);
+        clearInterval(intervalTerceraParte);
+
+        clearInterval(intervalCierreOjo);
+        clearInterval(intervalCierreMano);
+
+        clearInterval(intervalCierreCuartaParte);
+        clearInterval(intervalCierreTerceraParte);
+        clearInterval(intervalCierreSegundaParte);
 	}
 	anim.resume = function () {
 		document.getElementById('pause').disabled = false;
@@ -59,32 +71,64 @@ var animation = (function () {
 
         if(anim.timeConsumed > 1 && anim.timeConsumed < 150){
             anim.animacionPiramidePrimeraParte(150 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 300 && anim.timeConsumed < 450){
-            clearInterval(intervalPrimeraParte);
+        }else if(anim.timeConsumed > 150 && anim.timeConsumed < 450){
+            intervalPrimeraParte = setInterval(function(){
+                anim.animacionPiramideSegundaParte(150);
+            },(450 - anim.timeConsumed));
         }else if(anim.timeConsumed > 450 && anim.timeConsumed < 600){
             anim.animacionPiramideSegundaParte(600 - anim.timeConsumed);
         }else if(anim.timeConsumed > 600 && anim.timeConsumed < 900){
-            clearInterval(intervalSegundaParte);
+            intervalSegundaParte = setInterval(function(){
+                anim.animacionPiramideTerceraParte(150);
+            },(900 - anim.timeConsumed));
         }else if(anim.timeConsumed > 900 && anim.timeConsumed < 1050){
             anim.animacionPiramideTerceraParte(1050 - anim.timeConsumed);
         }else if(anim.timeConsumed > 1050 && anim.timeConsumed < 1350){
-            clearInterval(intervalTerceraParte);
+            intervalTerceraParte = setInterval(function(){
+                anim.animacionPiramideCuartaParte(150);
+            },(1350 - anim.timeConsumed));
         }else if(anim.timeConsumed > 1350 && anim.timeConsumed < 1500){
             anim.animacionPiramideCuartaParte(1500 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 1500 && anim.timeConsumed < 2500){
-            anim.animacionAperturaOjo(2500 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 2500 && anim.timeConsumed < 3700){
-            anim.animacionCierreOjo(3500 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 3500 && anim.timeConsumed < 4500){
-            anim.animacionAperturaOjo(4500 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 4500 && anim.timeConsumed < 5000){
-            anim.animacionMano(5000 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 5000 && anim.timeConsumed < 6000){
-            anim.animacionCierreMano(6000 - anim.timeConsumed);
-        }else if(anim.timeConsumed > 6000 && anim.timeConsumed < 7000){
-            anim.animacionCierreOjo(7000 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 1500 && anim.timeConsumed < 1650){
+            anim.animacionAperturaOjo(1650 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 1500 && anim.timeConsumed < 2000){
+            intervalCierreOjo = setInterval(function(){
+                anim.animacionCierreOjo(150,true);
+            },(2000 - anim.timeConsumed));
+        }else if(anim.timeConsumed > 2000 && anim.timeConsumed < 2150){
+            anim.animacionCierreOjo(2150 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 2150 && anim.timeConsumed < 2300){
+            anim.animacionAperturaOjo(2300 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 2300 && anim.timeConsumed < 2800){
+            anim.animacionMano(2800 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 2800 && anim.timeConsumed < 3300){
+            intervalCierreMano = setInterval(function(){
+                anim.animacionCierreMano(500);
+            },(3300 - anim.timeConsumed));
+        }else if(anim.timeConsumed > 3300 && anim.timeConsumed < 3800){
+            anim.animacionCierreMano(3800 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 3800 && anim.timeConsumed < 3950){
+            anim.animacionCierreOjo(3950 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 3950 && anim.timeConsumed < 4100){
+            anim.animacionCierrePiramideCuartaParte(4100 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 4100 && anim.timeConsumed < 4400){
+            intervalCierreCuartaParte = setInterval(function(){
+                anim.animacionCierrePiramideTerceraParte(150);
+            },(4400 - anim.timeConsumed));
+        }else if(anim.timeConsumed > 4400 && anim.timeConsumed < 4550){
+            anim.animacionCierrePiramideTerceraParte(4550 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 4550 && anim.timeConsumed < 4850){
+            intervalCierreTerceraParte = setInterval(function(){
+                anim.animacionCierrePiramideSegundaParte(150);
+            },(4850 - anim.timeConsumed));
+        }else if(anim.timeConsumed > 4850 && anim.timeConsumed < 5000){
+            anim.animacionCierrePiramideSegundaParte(5000 - anim.timeConsumed);
+        }else if(anim.timeConsumed > 5000 && anim.timeConsumed < 5300){
+            intervalCierreSegundaParte = setInterval(function(){
+                anim.animacionCierrePiramidePrimeraParte(150);
+            },(5300 - anim.timeConsumed));
         }else{
-            anim.animacionCierrePiramide(8000 - anim.timeConsumed);
+            anim.animacionCierrePiramidePrimeraParte(5450 - anim.timeConsumed);
         }
         this.timestapInit = new Date().getTime();
 	}
@@ -94,7 +138,7 @@ var animation = (function () {
     		bloqueOcultaPiramide.animate({transform:"t0,-31"}, ms, function(){
                 intervalPrimeraParte = setInterval(function(){
         			anim.animacionPiramideSegundaParte(150);
-                },450);
+                },300);
     		});
         }
 	}
@@ -103,7 +147,7 @@ var animation = (function () {
         clearInterval(intervalPrimeraParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-59"}, ms, function(){
-                intervalSegundaParte = setTimeout(function(){
+                intervalSegundaParte = setInterval(function(){
                     anim.animacionPiramideTerceraParte(150);
                 },300);
             });
@@ -111,9 +155,10 @@ var animation = (function () {
     }
 
     anim.animacionPiramideTerceraParte = function(ms){
+        clearInterval(intervalSegundaParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-87"}, ms, function(){
-                intervalTerceraParte = setTimeout(function(){
+                intervalTerceraParte = setInterval(function(){
                     anim.animacionPiramideCuartaParte(150);
                 },300);
             });
@@ -121,6 +166,7 @@ var animation = (function () {
     }
 
     anim.animacionPiramideCuartaParte = function(ms){
+        clearInterval(intervalTerceraParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-115"}, ms, function(){
                 anim.animacionAperturaOjo(150,true);
@@ -143,7 +189,7 @@ var animation = (function () {
     		bordeOjo.animate({d:"M 183.369,196.739 Q 154.669,170.548 125.437,196.739 Q 154.669,222.93 183.369,196.739"},ms, function(){
     			grupoPestanas.attr({display:"inline"});
     			if(primeraApertura){
-    				setTimeout(function(){
+    				intervalCierreOjo = setInterval(function(){
     					anim.animacionCierreOjo(150,true);
     				},500);
     			}else{
@@ -154,6 +200,7 @@ var animation = (function () {
 	}
 
 	anim.animacionCierreOjo = function(ms,primerCierre){
+        clearInterval(intervalCierreOjo);
         if(ms>1){
     		grupoPestanas.attr({display:"none"});
     		grupoOjoInt.animate({transform:"t0,0 s0"},(ms));
@@ -176,7 +223,7 @@ var animation = (function () {
 
     		grupoMano.attr({transform:"t0,0 s0"});
     		grupoMano.animate({transform:"t0,0 s1"},ms, mina.bounce, function(){
-                setTimeout(function(){
+                intervalCierreMano = setInterval(function(){
                     anim.animacionCierreMano(500);
                 },500);
     		});
@@ -184,6 +231,7 @@ var animation = (function () {
 	}
 
 	anim.animacionCierreMano = function(ms){
+        clearInterval(intervalCierreMano);
         if(ms>1){
     		grupoMano.animate({transform:"t0,0 s0"},ms, function(){
                 anim.animacionCierreOjo(150, false);
@@ -194,7 +242,7 @@ var animation = (function () {
     anim.animacionCierrePiramideCuartaParte = function(ms){
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-87"}, ms, function(){
-                setTimeout(function(){
+                intervalCierreCuartaParte = setInterval(function(){
                     anim.animacionCierrePiramideTerceraParte(150);
                 },300);
             });
@@ -202,9 +250,10 @@ var animation = (function () {
     }
 
     anim.animacionCierrePiramideTerceraParte = function(ms){
+        clearInterval(intervalCierreCuartaParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-59"}, ms, function(){
-                setTimeout(function(){
+                intervalCierreTerceraParte = setInterval(function(){
                     anim.animacionCierrePiramideSegundaParte(150);
                 },300);
             });
@@ -212,9 +261,10 @@ var animation = (function () {
     }
     
     anim.animacionCierrePiramideSegundaParte = function(ms){
+        clearInterval(intervalCierreTerceraParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,-31"}, ms, function(){
-                setTimeout(function(){
+                intervalCierreSegundaParte = setInterval(function(){
                     anim.animacionCierrePiramidePrimeraParte(150);
                 },300);
             });
@@ -222,6 +272,7 @@ var animation = (function () {
     }
 
     anim.animacionCierrePiramidePrimeraParte = function(ms){
+        clearInterval(intervalCierreSegundaParte);
         if(ms>1){
             bloqueOcultaPiramide.animate({transform:"t0,0"}, ms, function(){  
                 document.getElementById('play').disabled = false;
